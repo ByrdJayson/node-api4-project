762972a32a7f4e3b73cede002cf660bdc9a36d79
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-13 11:22:39 -0500

    GET /api/users returns array of user objects

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Users  = require('./users/users-modal.js')
 
 const server = express()
 server.use(express.json())
@@ -6,6 +7,16 @@ server.use(express.json())
 
 server.get('/', (req, res) => {
     res.send('<h2>Jaysons API Landing Page</h2>')
+})
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(() => {
+        res.status(500).json({message: "Internal Server Error!"})
+    })
 })
 
 
---
+++ api/users/users-modal.js
@@ -0,0 +1,49 @@
+
+
+const people = [
+    {
+    "id": "1",
+    "Name": "Jayson",
+    "Cohort": "Web 49",
+    "Password": "Bad!DONOTDO"
+    },
+    {
+    "id": "2",
+    "Name": "Chance",
+    "Cohort": "Web 49",
+    "Password": "Bad!DONOTDO"
+    },
+    {
+    "id": "3",
+    "Name": "Christopher",
+    "Cohort": "Web 49",
+    "Password": "Bad!DONOTDO"
+    },
+    {
+    "id": "4",
+    "Name": "Marco",
+    "Cohort": "Web 49",
+    "Password": "Bad!DONOTDO"
+    },
+    {
+    "id": "5",
+    "Name": "Angela",
+    "Cohort": "Web 49",
+    "Password": "Bad!DONOTDO"
+    },
+    {
+    "id": "6",
+    "Name": "Jennifer",
+    "Cohort": "Web 49",
+    "Password": "Bad!DONOTDO"
+    },
+]
+
+async function find() {
+    return people
+}
+
+
+module.exports = {
+    find
+}
