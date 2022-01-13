abf5bb3f2ec7ba976e80f3b7d9535dd226648a95
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-13 10:44:32 -0500

    Server Responds and has Landing at /

--- api/server.js
+++ api/server.js
@@ -3,4 +3,10 @@ const express = require('express')
 const server = express()
 server.use(express.json())
 
+
+server.get('/', (req, res) => {
+    res.send('<h2>Jaysons API Landing Page</h2>')
+})
+
+
 module.exports = server
